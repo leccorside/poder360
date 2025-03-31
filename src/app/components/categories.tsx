1fa36472454eb9c5f428bc9e817e3cfb0ad01e68
Johnathan <leccorside@gmail.com>
2025-03-31 01:42:49 -0300

    Correção página categorias

--- src/app/components/categories.tsx
+++ src/app/components/categories.tsx
@@ -7,7 +7,7 @@ export function Categories({ categories }:{categories: Category[]}){
       <ul className="text-[0.7rem] uppercase flex gap-2 flex-wrap">
         {categories.map((category: Category) => (
           <li key={category.id} className="flex flex-shrink-0">
-            <Link href={`/blog?categories=${category.slug}`}
+            <Link href={`/?categories=${category.slug}`}
             className="hover:underline border p-1 rounded-md">
               {category.name}
             </Link>
